b1fae663162be20a9dabfd21bdde587372310790
paitorre123 <paitorre@alumnos.ubiobio.cl>
2020-02-03 12:57:07 -0300

    Redireccionar mediante la clase  RedirectView("/form/showForm");

--- src/main/java/com/emanon/controladores/ControladorForm.java
+++ src/main/java/com/emanon/controladores/ControladorForm.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.emanon.model.Persona;
 
@@ -18,8 +19,8 @@ public class ControladorForm {
 	public static final String VIEW_RESULT_FORM= "resultForm";
 	
 	@GetMapping("/")
-	public String redirect() {
-		return "redirect:/form/showForm";
+	public RedirectView redirect() {
+		return new RedirectView("/form/showForm");
 	}
 	
 	
