1b3cb1d69e6b4d022b21a3ae3e8b093366c1bcb7
paitorre123 <paitorre@alumnos.ubiobio.cl>
2020-02-03 12:07:06 -0300

    Se pasan parametros por URL Tanto por parametro como por path

--- src/main/java/com/emanon/controladores/ControladorInit.java
+++ src/main/java/com/emanon/controladores/ControladorInit.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.emanon.model.Persona;
@@ -14,7 +16,7 @@ import com.emanon.model.Persona;
 @RequestMapping("/init")
 public class ControladorInit {
 	
-	public static final String VIEW_HELLO = "hello";
+	public static final String VIEW_HELLO = "hello2";
 	
 	
 	@GetMapping("/holaMundo")
@@ -27,6 +29,20 @@ public class ControladorInit {
 	public ModelAndView holaMundoMAV() {
 		ModelAndView modelo = new ModelAndView(VIEW_HELLO);
 		modelo.addObject("personas",getPersonas());
+		return modelo;
+	}
+	
+	@GetMapping("/request")
+	public ModelAndView requestPorParametro(@RequestParam(name="nb", required = false, defaultValue = "No name") String nombre) {
+		ModelAndView modelo = new ModelAndView(VIEW_HELLO);
+		modelo.addObject("nombre_in_model",nombre);
+		return modelo;
+	}
+	
+	@GetMapping("/request/{nb}")
+	public ModelAndView requestPorPath(@PathVariable("nb") String nombre) {
+		ModelAndView modelo = new ModelAndView(VIEW_HELLO);
+		modelo.addObject("nombre_in_model", nombre);
 		return modelo;
 	}
 	
